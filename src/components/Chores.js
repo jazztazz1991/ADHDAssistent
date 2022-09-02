7adab9475337b99ed0a9ff67b080d788a3909b8d
Cody Chase <cchase1991@gmail.com>
2022-09-02 19:15:57 -0400

    messing with forms

--- src/components/Chores.js
+++ src/components/Chores.js
@@ -3,7 +3,11 @@ import React from "react";
 function Chores() {
     return (
         <div>
-            <h1>ADHD Chores</h1>
+            <h1>Pick the chores that need to be done</h1>
+
+            <form>
+
+            </form>
         </div>
     );
 }
